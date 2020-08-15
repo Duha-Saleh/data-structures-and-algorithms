3238e0f9085161df35f14a102bcd12ec1c050fd2
Duha-Saleh <duhamahmoud096@gmail.com>
2020-08-15 22:20:22 +0300

    methode

--- code-challenges/methode/challenges-10.test.js
+++ code-challenges/methode/challenges-10.test.js
@@ -2,30 +2,29 @@
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
-
 Build a simple express server. Connect a '/hello' route that sends a greeting of your  choice. Connect a '/aboutme' route that sends a short bio about you to the front-end. Finally, connect a '/favoritefoods' route that sends an array to the front-end of your favorite foods. All other routes should respond with a status of 404.
 ------------------------------------------------------------------------------------------------ */
 
 const createServer = () => {
   // Solution code here...
+
   const express = require('express');
   const app = express();
-
   app.get('/hello',(req,res)=>{
-    res.send('its nice one :P')
-  })
+    res.status(200).send('Hello World');
+  });
+
   app.get('/aboutme',(req,res)=>{
-    res.send('I am Duha')
-  })
+    res.status(200).send('My name is Waleed how can I help you!');
+  });
+
   app.get('/favoritefoods',(req,res)=>{
-    res.send(['Mansaf','Maglobeh','Kabseh'])
-  })
+    res.status(200).send(['Everything','Everything']);
+  });
 
-  app.get('*',(req,res)=>{
-    res.status('404')
-  })
-//to here
-
+  app.get('/*',(req,res)=>{
+    res.status(404).send('Page not found');
+  });
   var server = app.listen(3301, function () {
     var port = server.address().port;
     console.log('Example app listening at port', port);
@@ -35,64 +34,52 @@ const createServer = () => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-
 Write a function named count that, given an integer and an array of arrays, uses either filter, map, or reduce to count the amount of times the integer is present in the array of arrays.
-
 Note: You might need to use the same method more than once.
-
 For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
   // Solution code here...
-
+  return input.reduce((a, row) => {
+    return a + row.filter(n => n === target).length;
+  }, 0)
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
-
 Write a function that, given an array of integer arrays as input, calculates the total sum of all the elements in the array.
-
 You may want to use filter, map, or reduce for this problem, but are not required to. You may need to use the same method more than once.
-
 For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
   // Solution code here...
-  var x = input.reduce( (a, b) => a.concat(b))
-  .reduce( (a, b) => a + b )
-
-  return x
-
-
+  return input.reduce((acc, row) => {
+    return acc + row.reduce((a, c) => a + c);
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
 Write a function named divisibleByFiveTwoToThePower that accepts an array of arrays as input.
-
 This function should first remove any elements that are not numbers or are not divisible by five.
-
 This function should then raise 2 to the power of the resulting numbers, returning an array of arrays.
-
 For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
   // Solution code here...
-  
+  return input.map(row => {
+    return row.filter(n => typeof n === 'number' && n % 5 === 0).map(n => 2 ** n);
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5 - Stetch Goal
-
 Write a function named findMaleAndFemale that, given the Star Wars data, below,
 returns the names of the characters whose gender is either male or female.
-
 The names should be combined into a single string with each character name separated by "and".
-
 For example, "C-3PO and Luke Skywalker".
 ------------------------------------------------------------------------------------------------ */
 
@@ -153,7 +140,6 @@ let findMaleAndFemale = (data) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6 - Stretch Goal
-
 Write a function named findShortest that, given the Star Wars data from Challenge 6, uses any combination of filter, map and reduce to return the name of the shortest character.
 ------------------------------------------------------------------------------------------------ */
 
@@ -163,13 +149,9 @@ let findShortest = (data) => {
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
 All the code below will verify that your functions are working to solve the challenges.
-
 DO NOT CHANGE any of the below code.
-
 Run your tests from the console: jest challenges-10.test.js
-
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
